3ab974c5b97dc2d98ad405b919c0935754c18c64
echoliu <root@echoliu.(none)>
2017-06-15 15:31:51 +0800

    delele all

--- blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from blog import models
-# Register your models here.
-
-admin.site.register(models.Blog)
-admin.site.register(models.Entry)
-admin.site.register(models.Author)
--- cmdb/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from cmdb import views
-
-urlpatterns = [
-    url(r'home/', views.home, {'user': 'user_ojb'}),
-]
